7f492e9723b6b35bea288d8a02ed6ded5dbbcd07
Samadsust71 <samadsust71@gmail.com>
2025-02-20 22:04:04 +0600

    added routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import AuthProvider from "./provider/AuthProvider.jsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/Router.jsx";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+     <AuthProvider>
+        <RouterProvider router={router} />
+        <Toaster />
+      </AuthProvider>
   </StrictMode>
 );
